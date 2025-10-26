263f293e15c07477112897debc45a15ff2ed998c
Leo <108789516+Boco14@users.noreply.github.com>
2025-10-26 15:51:55 +0000

    Created an object with constructors with parameters and methods

---
+++ const_param_package/ConstParam.java
@@ -0,0 +1,23 @@
+package const_param_package;
+import java.util.Scanner;
+
+class MyClass{
+    double Leo, Marc;
+
+    MyClass(double l, double m){
+        this.Leo = l;
+        this.Marc = m;
+    }
+    
+    void _method(){
+        double sum = Leo + Marc;
+        System.out.println("The sum of Leo and Marc is: " + sum);
+    }
+}
+
+public class ConstParam{
+    public static void main(String[] args){
+        MyClass sc = new MyClass(10.0, 11.0);
+        sc._method();
+    }
+}
