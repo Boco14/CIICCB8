70f4c76c46812a8591e65fdc15efde973bff504c
Leo <108789516+Boco14@users.noreply.github.com>
2025-10-29 08:07:07 +0000

    Added ternary if else

---
+++ Ternary_IfElse.java
@@ -0,0 +1,6 @@
+public class Ternary_IfElse{
+    public static void main(String[] args) {
+        int a = 3;
+        System.out.println((a >= 5 ) ? "a is greater than 5" : "Is not greater than 5");
+    }
+}
