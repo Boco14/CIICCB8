1a0a317247d85acd7626a0c2a2a8d6c485c508ff
Leo <108789516+Boco14@users.noreply.github.com>
2025-10-30 15:47:03 +0000

    Finishe the programming challenge - Number to month(1-12) and print number of days of that month

---
+++ programming_challenge/number_days/NumberDays.java
@@ -0,0 +1,16 @@
+package programming_challenge.number_days;
+import java.util.Scanner;
+
+class NumberDays {
+    void repeatPrompt(){
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter number of month: ");
+        int monthInput = scan.nextInt();
+
+        NumberDaysMonthInput monthObject = new NumberDaysMonthInput(monthInput);
+        monthObject.numberOfDays();
+
+        scan.close();
+    }
+}
---
+++ programming_challenge/number_days/NumberDaysDisplayMonth.java
@@ -0,0 +1,35 @@
+package programming_challenge.number_days;
+import java.time.YearMonth;
+
+class NumberDaysDisplayMonth {
+    int _index;
+    int year = 2025;
+    
+    String[] monthArray = {
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+    };
+
+    NumberDaysDisplayMonth(int i){
+        this._index = i;
+    }
+
+    void printMonth(){
+        // System.out.println("OPTIONS");
+        System.out.print("Month of " + monthArray[_index - 1]);
+
+        YearMonth yearMonth = YearMonth.of(year, _index);
+        int days = yearMonth.lengthOfMonth();
+        System.out.println(" has " + days + " days");
+    }
+}
