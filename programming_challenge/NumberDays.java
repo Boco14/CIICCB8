793250d07b2a25bb1093c6aca6a73d74de9d73bf
Leo <108789516+Boco14@users.noreply.github.com>
2025-10-30 09:34:47 +0000

    Finished NumberDays for programming challenge

--- programming_challenge/NumberDays.java
+++ programming_challenge/NumberDays.java
@@ -5,8 +5,7 @@
  * Assume that February has 28 days.
  * 
  * In this program, we use a switch-case statement to handle different month numbers entered by the user. 
- * The switch statement checks the value of the variable month , 
- * and executes the corresponding case block. 
+ * The switch statement checks the value of the variable "month" , and executes the corresponding case block. 
  * If none of the cases match, the default block is executed
  * 
  * @author Leo 
@@ -14,24 +13,47 @@
 
 package programming_challenge;
 import java.util.Scanner;
+import java.time.YearMonth;
 
 class Month{
-    int _month;
+    /**
+     * 1,3,5,7,8,10,12 - 31 days
+     * 4, 6, 9, 11 - 30 days
+     * 2 - 28/29 days on leap year
+     * 
+     * @param year 2025(final) and month(user)
+     * @return days of the month selected
+     * */ 
+    final int year = 2025;
+    final int month;
 
     Month(int m){
-        this._month = m;
+        this.month = m;
     }
 
     void numberOfDays(){
-        System.out.println("Number of month is: " + _month);
-
-        switch (_month){
-            case 1: 
-                System.out.println("Month of January");
-                break;
+        YearMonth yearMonth = YearMonth.of(year, month);
+        int daysOfMonth = yearMonth.lengthOfMonth();
+        
+        switch (month){
             default:
                 System.out.println("Not in the calendar");
+                break;
 
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12: 
+                System.out.println("Month of " + yearMonth.getMonth().toString().toLowerCase() + " " + daysOfMonth);
+                break;
         }
     }
 }
