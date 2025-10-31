52535b7b66a6b435a76f1478345efd7e48adf607
Leo <108789516+Boco14@users.noreply.github.com>
2025-10-31 23:31:54 +0800

    Finished programming  DaysOfWeek.java

---
+++ programming_challenge/DaysOfWeek.java
@@ -0,0 +1,61 @@
+/**
+ * Number to day of the week
+ * create a program that will accept 1-7 inputs and print the day in a week that corresponds with that number
+ *
+ * 1 - Monday
+ * 2 - Tuesday
+ * 3 - Wednesday
+ * 4 - Thursday
+ * 5 - Friday
+ * 6 - Saturday
+ * 7 - Sunday
+ *
+ * Any other number or character must print invalid input
+ *
+ * @author leo
+ */
+
+package programming_challenge;
+import java.util.Scanner;
+
+class WeekDays {
+    int selectDay;
+    String[] daysOfWeek = {
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    };
+
+    WeekDays(int sDay){
+        this.selectDay = sDay;
+    }
+    /**
+     * @param sday
+     * @return selected day
+     */
+    void displayDay(){
+        if(selectDay < 1 || selectDay > 7){
+            System.out.println("Not a valid answer");
+        }else{
+            System.out.println("Day: " + daysOfWeek[selectDay - 1]);
+        }
+    }
+}
+
+public class DaysOfWeek {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter a number 1-7 to display day of week: ");
+        int numberDay = scan.nextInt();
+
+        WeekDays weekDay = new WeekDays(numberDay);
+        weekDay.displayDay();
+
+        scan.close();
+    }
+}
--- programming_challenge/number_days/Main.java
+++ programming_challenge/number_days/Main.java
@@ -1,14 +1,14 @@
 /**
  * Number to month(1-12) and print number of days of that month
- * Write a program that prompts the user to enter a month number 
- * (1 for January, 2 for February, etc.) and outputs the number of days in that month. 
+ * Write a program that prompts the user to enter a month number
+ * (1 for January, 2 for February, etc.) and outputs the number of days in that month.
  * Assume that February has 28 days.
- * 
- * In this program, we use a switch-case statement to handle different month numbers entered by the user. 
- * The switch statement checks the value of the variable "month" , and executes the corresponding case block. 
+ *
+ * In this program, we use a switch-case statement to handle different month numbers entered by the user.
+ * The switch statement checks the value of the variable "month" , and executes the corresponding case block.
  * If none of the cases match, the default block is executed
- * 
- * @author Leo 
+ *
+ * @author Leo
  */
 
 package programming_challenge.number_days;
@@ -16,7 +16,7 @@ package programming_challenge.number_days;
 public class Main{
     public static void main(String[] args) {
         // Create Scanner object in memory
-       NumberDays repeat = new NumberDays();
-       repeat.repeatPrompt();
+    NumberDays repeat = new NumberDays();
+    repeat.repeatPrompt();
     }
 }
