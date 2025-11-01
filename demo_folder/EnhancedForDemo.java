73628be723303d6ad2fe528d502917021e65aaac
Leo <108789516+Boco14@users.noreply.github.com>
2025-11-02 00:32:59 +0800

    Update EnhancedForDemo.java
    - trying to comprehend enhance for loop or for-each-loop-statement

--- demo_folder/EnhancedForDemo.java
+++ demo_folder/EnhancedForDemo.java
@@ -2,8 +2,16 @@ package demo_folder;
 
 class EnhancedForDemo {
     public static void main(String[] args){
-        int[] numbers =
-                {1,2,3,4,5,6,7,8,9,10};
+        int[] numbers = {1, // item
+                        2, // item
+                        3, // item
+                        4, // item
+                        5, // item
+                        6, // item
+                        7, // item
+                        8, // item
+                        9, // item
+                        10}; // item
         for (int item : numbers) {
             System.out.println("Count is: " + item);
         }
