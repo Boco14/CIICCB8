3e942bccfbb6e272cdb8346a029f88483d075929
Leo <108789516+Boco14@users.noreply.github.com>
2025-11-02 09:47:13 +0800

    Update: tasksheet_1_1_3.java
    - Finished the task sheet 1-1-3 - Writing Control flow statements using Loops

--- demo_folder/BreakDemo.java
+++ demo_folder/BreakDemo.java
@@ -4,7 +4,7 @@ class BreakDemo {
     public static void main(String[] args) {
 
         int[] arrayOfInts =
-                { 32, 87, 3, 589,
+                        { 32, 87, 3, 589,
                         12, 1076, 2000,
                         8, 622, 127 };
         int searchfor = 12;
--- programming_challenge/SumOfInteger.java
+++ programming_challenge/SumOfInteger.java
@@ -40,5 +40,6 @@ public class SumOfInteger {
                     break;
                 }
         }
+        scan.close();
     }
 }
