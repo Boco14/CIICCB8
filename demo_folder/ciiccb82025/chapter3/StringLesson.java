bc937fbe9e1cffe7324d3e9d6bf97976021b3410
Leo <108789516+Boco14@users.noreply.github.com>
2025-11-08 09:35:58 +0800

    Update: StringLesson.java

--- demo_folder/ciiccb82025/chapter3/StringLesson.java
+++ demo_folder/ciiccb82025/chapter3/StringLesson.java
@@ -11,7 +11,6 @@ public class StringLesson {
         String name2 = "fluffy"; // creates a new string "name2" with the same address as "name"
         name = null;
         name2 = null;
-
         String name3 = new String("fluffy");
         System.out.println(a);
 
@@ -19,27 +18,68 @@ public class StringLesson {
         s1 = "1";
         s2 = s1.concat("2");
         s2.concat("3");
-
         System.out.println(s2);
 
+        System.out.println();
+
         String string = "animals";
-        char[] chars = new char[4]; // make space
+        char[] chars = new char[7]; // make space
 
         System.out.println(string.length()); // 7
-        string.getChars(0, 3, chars, 0);
+        string.getChars(0, 7, chars, 0);
         // 0 - srcBegin
         // 3 - srcEnd
         // char - assign to the char[]
         System.out.println(chars);
-        /*
-        int[] array = {1,2,3,4,5};
 
-        for(int x : array){
-            for(int y : array){
-                System.out.print(y + "c ");
-            }
-            System.out.println(x + "r ");
-        }
-        */
+//      String string = "animals"
+        // chartAt() gets the character at specific index
+        System.out.print(string.charAt(0) + ", "); // a
+        System.out.println(string.charAt(6)); // s
+
+        System.out.println();
+
+        // indexOf() gets the index of the specified char
+        System.out.println(string.indexOf('a')); // 0
+        System.out.println(string.indexOf("al")); // 4
+        System.out.println(string.indexOf('a', 4)); // 4
+        System.out.println(string.indexOf("al", 5)); // -1 does not throw an exception but now match was found -(-1)
+        System.out.println(string.indexOf("al", 5 - 1)); // 4
+
+        System.out.println();
+
+        // substring() returns parts of the string
+        System.out.println(string.substring(3)); // mals
+        System.out.println(string.substring(string.indexOf('m'))); // mals
+        System.out.println(string.substring(3,4)); // m
+        System.out.println(string.substring(3,7)); // mals
+        /*  System.out.println(string.substring(3,2));
+            throws an exception endIndex should be larger than start */
+
+        System.out.println();
+
+        // toLowerCase() returns the string as lowercased
+        System.out.println(string.toLowerCase());
+
+        // toUpperCase() returns the string to uppercase
+        System.out.println(string.toUpperCase());
+        System.out.println("AbcdEf123".toUpperCase());
+
+        System.out.println();
+
+        // equals() checks if the Two String Objects contain the same characters in the same order
+        System.out.println("abc".equals("ABC")); // false
+        System.out.println("ABC".equals("ABC")); // true
+
+        // equalsIgnoreCase() always true whether both of the string is lower or uppercase - converts the character case when needed
+        System.out.println("abc".equalsIgnoreCase("ABC")); // true
+
+        System.out.println();
+
+        // startsWith() and endsWith() looks at value if it matches a part of string
+        System.out.println("abc".startsWith("a")); // true
+        System.out.println("abc".startsWith("A")); // false
+        System.out.println("abc".endsWith("c")); // true
+        System.out.println("abc".endsWith("C")); // false
     }
 }
