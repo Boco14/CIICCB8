ffeb2cdc48da585a88dfa7ad94ccd1368cff7978
Leo <108789516+Boco14@users.noreply.github.com>
2025-11-12 16:09:59 +0800

    Apply .gitignore rules

--- demo_folder/ForDemo.java
+++
@@ -1,9 +0,0 @@
-package demo_folder;
-
-class ForDemo {
-    public static void main(String[] args){
-        for(int i=1; i<11; i++){
-            System.out.println("Count is: " + i);
-        }
-    }
-}
