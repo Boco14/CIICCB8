9dab9183f8218047094113dfbd8edcbaee224c22
Leo <108789516+Boco14@users.noreply.github.com>
2025-11-17 19:37:36 +0800

    Update BinarySearch.java

--- algorithms/BinarySearch.java
+++ algorithms/BinarySearch.java
@@ -6,32 +6,35 @@ public class BinarySearch {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] arrayL = {10,5,8,2,9,1,6,3,7,4};
+        int[] arrayL = {10, 5, 8, 2, 9, 1, 6, 3, 7, 4};
         Arrays.sort(arrayL);
-        System.out.println(Arrays.toString(arrayL));
 
         System.out.print("Find index of number: ");
-        int find = scan.nextInt();
-        FindNumber findNumber = new FindNumber(find);
-        int start=0,mid=0,end=0,
-            length = arrayL.length;
-        inputInfo(new int[]{arrayL[0]}, start, length, mid, end);
-        findNumber.displayIndex();
+        int input = scan.nextInt();
+
+        int length = arrayL.length;
+        int start = 1,
+            mid = length / 2,
+            end = length;
+
+        for(int x : arrayL)
+            System.out.print(arrayL[x-1] + " ");
+
+        System.out.println();
+        if (input >= start && input <= mid) {
+            System.out.println("Input is between " + start + " and " + mid);
+            index(input, start, mid, arrayL);
+        }
+        else if (input >= mid && input <= end) {
+            System.out.println("Input is between " + mid + " and " + end);
+            index(input, mid, end, arrayL);
+        }
+        else System.out.println("Input is not valid");
     }
-    static void inputInfo(int[] arrayL, int start, int length, int mid, int end){
-        mid = length / 2;
-        end = length;
-        System.out.println("Start " + arrayL[start] + ", Middle " + mid + ", End " + end);
+    public static int index(int target, int param1, int param2, int[] arrParam){
+        System.out.println("Constructor check");
+        System.out.println(arrParam[param1-1] + ", " + arrParam[param2-1]);
+        System.out.println(arrParam[target]);
+        return 0;
     }
 }
- class FindNumber{
-    int input;
-    public FindNumber(int i){
-        this.input = i;
-    }
-    void displayIndex(){
-        System.out.println();
-
-        System.out.println("--- Output here -> " + input);
-    }
- }
