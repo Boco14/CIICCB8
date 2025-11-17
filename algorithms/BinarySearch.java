295cd43aaf07760d08e9fe87b99830c3d9896b2f
Leo <108789516+Boco14@users.noreply.github.com>
2025-11-17 16:08:24 +0800

    Update BinarySearch.java

--- algorithms/BinarySearch.java
+++ algorithms/BinarySearch.java
@@ -5,23 +5,21 @@ import java.util.Scanner;
 public class BinarySearch {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int[] arrayL = {10,5,8,2,9,1,6,3,7,4};
         Arrays.sort(arrayL);
         System.out.println(Arrays.toString(arrayL));
 
-        int start=1,
-            mid=0,
-            end=0,
-            lenght = arrayL.length;
-
-        if(lenght %2 == 0){
-            System.out.println("Lenght is " + lenght + " and divisible by 2");
-            mid = lenght / 2;
-            System.out.println("Start " + Arrays[start] + "Middle " + mid) + "End ";
-        }
+        System.out.print("Find number: ");
+        int find = scan.nextInt();
+        int start=arrayL[0],mid=0,end=0,
+            length = arrayL.length;
+        inputInfo(arrayL, start, length, mid, end);
     }
-
-    public void numberInput(){
-        return;
+    static void inputInfo(int[] arrayL, int start, int length, int mid, int end){
+        System.out.println("length is " + length + " and divisible by 2");
+        mid = length / 2;
+        end = length;
+        System.out.println("Start " + arrayL[start-1] + "\nMiddle " + mid + "\nEnd " + end);
     }
 }
