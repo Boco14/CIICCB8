7c1795725f164753e4cfa02db6a184d4ac647f21
Leo <108789516+Boco14@users.noreply.github.com>
2025-12-11 15:52:25 +0800

    Update: Task4.java

--- tasks_package/Task4.java
+++ tasks_package/Task4.java
@@ -1,39 +1,28 @@
 package tasks_package;
 import output.ProgramTerminated;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Task4 {
     private static Scanner scan = new Scanner(System.in);
-    private static String input;
 
-    static void main() {
+    public static void main(String[] args) {
         System.out.print("Input string: ");
         String input = scan.next();
 
-        withStringBuilder(input);
-        ProgramTerminated.print();
-    }
+        ReversedString rs = i -> i;
+        StringBuilder sb = new StringBuilder(rs.userInput(input));
 
-    static void withStringBuilder(String input){
-        StringBuilder sb = new StringBuilder(input);
-        System.out.println((input.equals(String.valueOf(sb.reverse()))) ? "The input string is a palindrome." : "The input string is not a palindrome.");
-    }
-
-    static void palindromeNoStringBuilder(String input){
-        ArrayList<String> reversed = new ArrayList<>();
-        int start = input.length();
-        String reversedString = "";
-
-        System.out.print("Reversed string: ");
-        for(int x=start; x >= 1; x--){
-            char c = input.charAt(x-1);
-            System.out.print(c);
-            reversed.add(String.valueOf(c));
-            reversedString += c;
+        // Check palindrome
+        if (input.equals(sb.reverse().toString())) {
+            System.out.println("The input string is a palindrome.");
+        } else {
+            System.out.println("The input string is not a palindrome.");
         }
 
-        System.out.println();
-        System.out.println(input.equals(reversedString) ? "Palindrome" : "Not a palindrome");
+        ProgramTerminated.print();
     }
+}
+
+interface ReversedString {
+    String userInput(String input);
 }
